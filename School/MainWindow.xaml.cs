d11002f0baebfafac866472f40537fd543ce68ef
Jay Alexander <jalexander@synergetic.net.au>
2019-03-25 15:41:39 +1100

    MOD 2 - refactoring, saving to database, and simple common error handling

--- School/MainWindow.xaml.cs
+++ School/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Data;
 using System.Windows.Input;
 using School.Data;
 using System.Globalization;
+using System.Data;
+using System.Data.Objects;
 
 namespace School
 {
@@ -57,70 +59,103 @@ namespace School
         // When the user presses a key, determine whether to add a new student to a class, remove a student from a class, or modify the details of a student
         private void studentsList_KeyDown(object sender, KeyEventArgs e)
         {
-            // TODO: Exercise 1: Task 1a: If the user pressed Enter, edit the details for the currently selected student
             switch (e.Key)
             {
                 case Key.Enter: Student student = this.studentsList.SelectedItem as Student;
-
-                    StudentForm sf = new StudentForm();
-
-                    sf.Title = "Edit Student Details";
-                    sf.firstName.Text = student.FirstName;
-                    sf.lastName.Text = student.LastName;
-                    sf.dateOfBirth.Text = student.DateOfBirth.ToString("d");
-
-                    if (sf.ShowDialog().Value)
-                    {
-                        student.FirstName = sf.firstName.Text;
-                        student.LastName = sf.lastName.Text;
-                        student.DateOfBirth = DateTime.ParseExact(sf.dateOfBirth.Text, "MM/dd/yyyy", CultureInfo.InvariantCulture);
-                        saveChanges.IsEnabled = true;
-                    }
+                    EditStudent(student);
                     break;
-                // TODO: Exercise 1: Task 2a: Use the StudentsForm to display and edit the details of the student
+
                 case Key.Insert:
-
-                    sf = new StudentForm();
-                    // TODO: Exercise 1: Task 2b: Set the title of the form and populate the fields on the form with the details of the student
-                    sf.Title = "New Student for Class " + teacher.Class;
-                    
-                    if (sf.ShowDialog().Value)
-                    {
-                        Student newStudent = new Student();
-                        newStudent.FirstName = sf.firstName.Text;
-                        newStudent.LastName = sf.lastName.Text;
-                        newStudent.DateOfBirth = DateTime.ParseExact(sf.dateOfBirth.Text, "MM/dd/yyyy", CultureInfo.InvariantCulture);
-
-                        this.teacher.Students.Add(newStudent);
-
-                        this.studentsInfo.Add(newStudent);
-
-                        saveChanges.IsEnabled = true;
-                    }
+                    AddNewStudent();
                     break;
 
                 case Key.Delete: student = this.studentsList.SelectedItem as Student;
-
-                    // TODO: Exercise 1: Task 3a: Display the form
-                    // TODO: Exercise 1: Task 3b: When the user closes the form, copy the details back to the student
-                    MessageBoxResult response = MessageBox.Show(
-                        String.Format("Remove {0}?", student.FirstName + " " + student.LastName),
-                        "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question,
-                        MessageBoxResult.No);
-
-                    if (response == MessageBoxResult.Yes)
-                    {
-                        this.schoolContext.Students.DeleteObject(student);
-                        // TODO: Exercise 1: Task 3c: Enable saving (changes are not made permanent until they are written back to the database)
-                        saveChanges.IsEnabled = true;
-                    }
+                    RemoveStudent(student);
                     break;
-
             }
-                       
+        }
+
+        private void studentsList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Student student = this.studentsList.SelectedItem as Student;
+            EditStudent(student);
+        }
+
+        private void AddNewStudent()
+        {
+            StudentForm sf = new StudentForm();
             
-            
-           
+            sf.Title = "New Student for Class " + teacher.Class;
+
+            if (sf.ShowDialog().Value)
+            {
+                Student newStudent = new Student();
+                newStudent.FirstName = sf.firstName.Text;
+                newStudent.LastName = sf.lastName.Text;
+                newStudent.DateOfBirth = DateTime.ParseExact(sf.dateOfBirth.Text, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+                this.teacher.Students.Add(newStudent);
+                this.studentsInfo.Add(newStudent);
+                saveChanges.IsEnabled = true;
+            }
+        }
+
+        private void RemoveStudent(Student student)
+        {
+            MessageBoxResult response = MessageBox.Show(
+                String.Format("Remove {0}?", student.FirstName + " " + student.LastName),
+                "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question,
+                MessageBoxResult.No);
+
+            if (response == MessageBoxResult.Yes)
+            {
+                this.schoolContext.Students.DeleteObject(student);
+                saveChanges.IsEnabled = true;
+            }
+        }
+
+        private void EditStudent(Student student)
+        {
+            StudentForm sf = new StudentForm();
+
+            sf.Title = "Edit Student Details";
+            sf.firstName.Text = student.FirstName;
+            sf.lastName.Text = student.LastName;
+            sf.dateOfBirth.Text = student.DateOfBirth.ToString("d");
+
+            if (sf.ShowDialog().Value)
+            {
+                student.FirstName = sf.firstName.Text;
+                student.LastName = sf.lastName.Text;
+                student.DateOfBirth = DateTime.ParseExact(sf.dateOfBirth.Text, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+                saveChanges.IsEnabled = true;
+            }
+        }
+
+        private void saveChanges_onClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.schoolContext.SaveChanges();
+                saveChanges.IsEnabled = false;
+            }
+            catch (OptimisticConcurrencyException)
+            {
+                // If another user has changed the same students earlier, then overwrite their changes with the new data
+                this.schoolContext.Refresh(RefreshMode.ClientWins, schoolContext.Students);
+                this.schoolContext.SaveChanges();
+            }
+            catch (UpdateException uEx)
+            {
+                // If some sort of database exception has occurred, then display the reason for the exception and rollback
+                MessageBox.Show(uEx.InnerException.Message, "Error saving changes");
+                this.schoolContext.Refresh(RefreshMode.StoreWins, schoolContext.Students);
+            }
+            catch (Exception ex)
+            {
+                // If some other exception occurs, report it to the user
+                MessageBox.Show(ex.Message, "Error saving changes");
+                this.schoolContext.Refresh(RefreshMode.ClientWins, schoolContext.Students);
+            }
         }
 
         #region Predefined code
--- School/StudentForm.xaml.cs
+++ School/StudentForm.xaml.cs
@@ -21,5 +21,36 @@ namespace School
         }
 
         #endregion
+
+        private void ok_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(this.firstName.Text))
+            {
+                MessageBox.Show("The student must have a first name", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (String.IsNullOrEmpty(this.lastName.Text))
+            {
+                MessageBox.Show("The student must have a last name", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            DateTime result;
+            if (!DateTime.TryParse(this.dateOfBirth.Text, out result))
+            {
+                MessageBox.Show("The date of birth must be a valid date", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            TimeSpan age = DateTime.Now.Subtract(result);
+            if (age.Days / 362.25 < 5)
+            {
+                MessageBox.Show("The student must be at least 5 years old", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            this.DialogResult = true;
+        }
     }
 }
